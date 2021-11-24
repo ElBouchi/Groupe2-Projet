5498f0f21b3a2a012aab61723d9615863dbfc0c0
Nazim GAOUA <nazim.gaoua.dz@viacesi.fr>
2021-11-24 12:31:11 +0100

    création de la sauvegarde complete

---
+++ ISauvegarde.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Projet
+{
+    interface ISauvegarde
+    {
+        void Sauvegarde(string sourcePATH, string destPATH, bool copyDirs, bool createFirstFolder, long fileCount, int getIndex, string getName);
+    }
+}
--- Travail.cs
+++ Travail.cs
@@ -1,16 +1,12 @@
-﻿using Newtonsoft.Json;
-using System;
-using System.IO;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using ConsoleTables;
-using System.Linq;
 
 namespace Projet
 {
     class Travail
     {
-        public static string filePath = @"C:\Users\Nazim GAOUA\source\repos\Groupe2-Projett\state.json";
+        public static string filePath = @"..\..\..\work.json";
         public string name { get; set; }
         public string repS { get; set; }
         public string repC { get; set; }
